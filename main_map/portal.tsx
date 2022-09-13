a9c27a721ecf4622ef2bd381aa43996e898ed0eb
Robin Avci <50947565+RobinWilliamAvci@users.noreply.github.com>
2022-09-13 11:22:48 +0200

    Add files via upload

---
+++ main_map/portal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="portal" tilewidth="30" tileheight="30" tilecount="100" columns="10">
+ <image source="../../../../Downloads/portal.png" width="300" height="300"/>
+</tileset>
